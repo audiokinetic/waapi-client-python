540f431a260177bd72a4c0b3a3db94585c749d8d
Samuel Longchamps <slongchamps@audiokinetic.com>
2018-08-14 17:58:04 -0400

    Prepared the repo for PyPi publishing
    * Fixed the package inclusion to exclude the tests
    * Fixed setup.py to have all required fields and the readme as markdown
    * Moved out the interactive client from the tests
    
    Tested with PyPi test server (https://test.pypi.org/project/waapi-client/)
    
    Change-Id: I7fd15fef37c4ccde932b032bf51daa58055de1ad

--- setup.py
+++ setup.py
@@ -1,28 +1,39 @@
+import os
 from distutils.core import setup
 
 from setuptools import find_packages
 
+# Dump the README.md file
+with open(os.path.join(os.path.abspath(os.path.dirname(__file__)), 'README.md')) as f:
+    long_description = f.read()
+
 setup(
-    name = 'waapi-client',
-    packages = find_packages(exclude=['waapi/test']),
+    name='waapi-client',
+    packages=find_packages(exclude=['waapi.test']),
+    test_suite='waapi.test',
     install_requires=[
       'autobahn',
       'six'
     ],
+    license='Apache License 2.0',
+    platforms=['any'],
     scripts=[],
-    version = '0.1.0',
-    description = 'Wwise Authoring API client.',
-    author = 'Audiokinetic',
-    maintainer = 'Samuel Longchamps',
-    maintainer_email = 'slongchamps@audiokinetic.ca',
-    url = 'https://github.com/audiokinetic/waapi-client-python',
-    download_url = 'https://github.com/audiokinetic/waapi-client-python/releases',
-    keywords = ['waapi', 'wwise', 'audiokinetic'],
-    classifiers = [
+    version='0.1b1',
+    description='Wwise Authoring API client.',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    author='Audiokinetic',
+    author_email='sales@audiokinetic.com',
+    maintainer='Samuel Longchamps',
+    maintainer_email='slongchamps@audiokinetic.ca',
+    url='https://github.com/audiokinetic/waapi-client-python',
+    download_url='https://github.com/audiokinetic/waapi-client-python/releases',
+    keywords=['waapi', 'wwise', 'audiokinetic'],
+    classifiers=[
         'Intended Audience :: Developers',
         'Programming Language :: Python :: 3',
         'License :: OSI Approved :: Apache Software License',
         'Topic :: Software Development :: Libraries :: Python Modules',
-        'Development Status :: 3 - Alpha'
+        'Development Status :: 4 - Beta'
     ],
 )
